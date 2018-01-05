9dd21a6ecfdf634e1b9c7bfe6c8f3daf3d7a11d0
Taha2040Qureshi <taha2040qureshi@gmail.com>
2018-01-05 10:55:54 +0500

    adding other pages in navebar

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
   
+  get 'contacts/index'
+
+  get 'prices/index'
+
+  get 'packages/index'
+
+  get 'services/index'
+
+  get 'abouts/index'
+
   root 'home#index'
   
   devise_for :users
