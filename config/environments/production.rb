e97b6880cc04d9b616f30a26cc27b6ea055d1ab9
Taha2040Qureshi <taha2040qureshi@gmail.com>
2018-01-13 14:53:10 +0500

    sending email on contact creation finxing issues

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,7 +89,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { :host => 'seowave.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'techsavvy-web.herokuapp.com' }
 
   # Email Configuration for Gmail
   config.action_mailer.smtp_settings = {
