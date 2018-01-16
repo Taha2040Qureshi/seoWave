3d84aab703054e6621f07720acb6a28ce9356846
codi302 <m.rehmangames@gmail.com>
2018-01-17 02:11:10 +0500

    before slider empty commit

--- app/mailers/contacts_mailer.rb
+++ app/mailers/contacts_mailer.rb
@@ -2,7 +2,7 @@ class ContactsMailer < ApplicationMailer
 
   def send_email_on_contact_creation(contact)
     @contact = contact
-    email = "m.rehmangames@gmail.com"
+    email = "Customer.support@techsavvy-global.com"
     mail(to: email , subject: 'New Contact Share your idea')
   end
 
